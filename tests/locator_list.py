d4e84e9c3cedb821d6390a853b3c6275b6fcd947
Elena Pet <dragonqueen3@yandex.ru>
2025-02-25 13:25:20 +0700

    rewrite lockators

--- tests/locator_list.py
+++ tests/locator_list.py
@@ -1,23 +1,12 @@
 lk_button = .// p[text() = 'Личный Кабинет'] # кнопка "Личный кабинет"
-#.// a[text() = 'Зарегистрироваться'] # кнопка "зарегистрироваться" на странице "Вход"
-#.//button[text()='Войти в аккаунт']    .//button[0]   #  кнопка "Войти в аккаунт"
 
 reg_button = driver.find_element(By.LINK_TEXT, 'Зарегистрироваться') # кнопка "зарегистрироваться" в форме логин в форме регистрации
-#.//input #поля ввода в форме регистрации "имя", "почта", "пароль"
+
 login_reg = driver.find_element(By.XPATH,".//a[text()='Войти']") #кнопка  "Войти" в форме входа для зарегистрированных пользователей
 exit_button = driver.find_element(By.XPATH, ".//button[text()='Выход']") # кнопка выход из аккаунта
-#.//p[@class='input__error text_type_main-default'] #сообщение о некорректном пароле
-#.//button[0] # Кнопка входа в аккаунт на главной странице
-#.//a[(@href='https://stellarburgers.nomoreparties.site/forgot-password') or (text()='Восстановить пароль')] #кнопка "Восстановить пароль"
-#.//a[(@href='https://stellarburgers.nomoreparties.site/login') or text()='Войти']#кнопка"войти" на страницке восстановления пароля
-#.//p[(contains(@class, 'AppHeader_header')) or (text()='Конструктор')] #иконка и подпись "конструктор"
-#.//svg
-#//div/a/* #логотип Stellar Burgers
-#.//img[contains(@alt, 'булка')] # список изображений булок
+
 switch_souses = driver.find_element(By.XPATH, ".//span[text()='Соусы']/parent::*") # кнопка конструктора "Соусы"
-#.//img[contains(@alt, 'Соус')] #  соусы в конструкторе
-#.//span[text()='Булки']/parent::* # кнопка конструктора "Булки"
-#.//span[text()='Начинки']/parent::* #кнопка конструктора "Начинки"
+
 ingredients = driver.find_elements(By.XPATH, ".//ul[@class='BurgerIngredients_ingredients__list__2A-mT']") # все ингридиенты в конструкторе. 0 - булки, 1- соусы, 2 - начинки
 elements_souses = ingredients[1] # список соусов
 current_scroll = driver.find_elements(By.XPATH,".//*[contains(@class, 'current')]") #
